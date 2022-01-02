550aee9343518b097213f03dc4cd2bfd07ed9a51
woohwan <whpark70@naver.com>
2022-01-02 17:24:24 +0900

    Initialize Vagrant Repository

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# ansible 설치
+yum install epel-release -y
+yum install ansible -y
+
+# 환경설정 초기 파일 구성 for vagrant only
+mkdir -p /home/vagrant/.vim/autoload  /home/vagrant/.vim/bundle
+touch /home/vagrant/.vimrc
+touch /home/vagrant/.bashrc
